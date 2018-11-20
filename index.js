c855ba8e910fc795ae3860428b29138836dbe114
Joel Hooks <joel@egghead.io>
2018-11-19 17:03:40 -0800

    try a better wild card

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000)
 
-app.use(express.static(__dirname + '/_book'));
+app.use(express.static(__dirname + '/_book'))
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
-app.get('/*', function(req,res){
-  res.redirect('https://howtoegghead.com/instructors', '301');
-});
+app.get('*', function(req, res) {
+  res.redirect('https://howtoegghead.com/instructors', '301')
+})
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+  console.log('Node app is running on port', app.get('port'))
+})
